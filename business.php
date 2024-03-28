f5d79c92fe16f6f5eb7c5963c0268e52df44f0bb
mayankacsius <145326479+mayankacsius@users.noreply.github.com>
2024-03-28 18:52:43 +0530

    sad

--- business.php
+++ business.php
@@ -5,7 +5,7 @@ namespace App\Asic;
 class business
 {
 
-  /**
+    /**
      * @var date $startDate
      */
     protected $startDate = null;
@@ -17,31 +17,16 @@ class business
 
     /**
      * @var trueType $agentAppointed
-
      */
     protected $agentAppointed = null;
- 
-    /**
-     * @var nameIndexEntityType $organisation
-     */
-     protected $organisation = null;
 
     /**
-     * @var individualType $person
-     */
-    protected $person = null;
-
-    /**
-     * @param nameIndexEntityType $organisation
-     * @param individualType $person
      * @param date $startDate
      * @param date $endDate
      * @param trueType $agentAppointed
      */
-    public function __construct($organisation = null, $person = null, $startDate = null, $endDate = null, $agentAppointed = null)
+    public function __construct($startDate, $endDate, $agentAppointed)
     {
-      $this->organisation = $organisation;
-      $this->person = $person;
       $this->startDate = $startDate;
       $this->endDate = $endDate;
       $this->agentAppointed = $agentAppointed;
@@ -98,42 +83,6 @@ class business
     public function setAgentAppointed($agentAppointed)
     {
       $this->agentAppointed = $agentAppointed;
-      return $this;
-    }
-
-    /**
-     * @return nameIndexEntityType
-     */
-    public function getOrganisation()
-    {
-      return $this->organisation;
-    }
-
-    /**
-     * @param nameIndexEntityType $organisation
-     * @return \App\Asic\business
-     */
-    public function setOrganisation($organisation)
-    {
-      $this->organisation = $organisation;
-      return $this;
-    }
-
-    /**
-     * @return individualType
-     */
-    public function getPerson()
-    {
-      return $this->person;
-    }
-
-    /**
-     * @param individualType $person
-     * @return \App\Asic\business
-     */
-    public function setPerson($person)
-    {
-      $this->person = $person;
       return $this;
     }
 
--- identifierType.php
+++ identifierType.php
@@ -17,7 +17,6 @@ class identifierType
 
     /**
      * @var issuingPartyCodeType $issuingPartyCode
-
      */
     protected $issuingPartyCode = null;
 
@@ -42,24 +41,10 @@ class identifierType
     protected $deleteIndicator = null;
 
     /**
-     * @var numberHeadingType $numberHeading
-     */
-    protected $numberHeading = null;
-
-    /**
-     * @var nniNumberType $number
-     */
-    protected $number = null;
-
-    /**
-     * @param numberHeadingType $numberHeading
-     * @param nniNumberType $number
      * @param identifierValueType $identifierValue
      */
-    public function __construct($numberHeading = null, $number = null, $identifierValue = null)
+    public function __construct($identifierValue)
     {
-      $this->numberHeading = $numberHeading;
-      $this->number = $number;
       $this->identifierValue = $identifierValue;
     }
 
@@ -186,42 +171,6 @@ class identifierType
     public function setDeleteIndicator($deleteIndicator)
     {
       $this->deleteIndicator = $deleteIndicator;
-      return $this;
-    }
-
-    /**
-     * @return numberHeadingType
-     */
-    public function getNumberHeading()
-    {
-      return $this->numberHeading;
-    }
-
-    /**
-     * @param numberHeadingType $numberHeading
-     * @return \App\Asic\identifierType
-     */
-    public function setNumberHeading($numberHeading)
-    {
-      $this->numberHeading = $numberHeading;
-      return $this;
-    }
-
-    /**
-     * @return nniNumberType
-     */
-    public function getNumber()
-    {
-      return $this->number;
-    }
-
-    /**
-     * @param nniNumberType $number
-     * @return \App\Asic\identifierType
-     */
-    public function setNumber($number)
-    {
-      $this->number = $number;
       return $this;
     }
 
